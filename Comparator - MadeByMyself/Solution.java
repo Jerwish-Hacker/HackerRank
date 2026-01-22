86f18b17b485fe5e88cbcbe7dc6754d3332d8647
Justin <justinraj958@gmail.com>
2026-01-22 23:10:51 +0530

    Comparator to sort the string arr by string length

--- Comparator - MadeByMyself/Solution.java
+++ Comparator - MadeByMyself/Solution.java
@@ -1,18 +1,24 @@
 import java.io.*;
 import java.util.*;
+
 // Comparator
 public class Solution { 
       public static void main(String[] args){
-          List<Integer> arr =  Arrays.asList(85,22, 23, 14, 95);          
-          Collections.sort(arr,new Comparator<Integer>(){             
-            @Override
-            public int compare(Integer a,Integer b){
-              if(a>b){
+          String[] arr = {"Justin" , "Ravi", "Rohit" , "AJ"};          
+          Arrays.sort(arr,new Comparator<String>(){
+          public int compare(String s1,String s2){
+              if(s1.length() > s2.length()){
                 return 1;
+              }              
+              else if(s1.length() < s2.length()){
+                return -1;
               }
-              return -1;
-            }              
+               return 0;
+          }          
           });
-          System.out.println(arr.toString());
+          
+          for(String s: arr){
+              System.out.println(s);
+          }
       }  
 }
