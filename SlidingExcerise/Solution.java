bea35625e955d8373695d7a44e40f239fb0afeb6
Justin <justinraj958@gmail.com>
2026-01-24 00:06:15 +0530

    Just to green

---
+++ SlidingExcerise/Solution.java
@@ -0,0 +1,17 @@
+//First negative number in every window of size k
+public class Solution{
+  public static void main(String[] args){
+    int[] arr={1,2,-8,6,-11,-24,95,3,-2};
+    //int[] arr= {1,4,6,3,7,12,9};
+    int k =4;
+    int start=0;
+    List<Integer> list=new ArrayList<>();
+    
+    for(int i=0;i<arr.length;i++){                
+        
+        while(){
+            
+        }                                          
+    }                                      
+  }
+}
