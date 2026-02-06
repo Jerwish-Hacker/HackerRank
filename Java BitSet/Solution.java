efb2172609dc8e530fb7bb98065d2e90d1275fd6
Justin <justinraj958@gmail.com>
2026-02-06 21:16:12 +0530

    BitSet part-3

--- Java BitSet/Solution.java
+++ Java BitSet/Solution.java
@@ -12,12 +12,44 @@ public class Solution {
         
         scanner.nextLine();
         
-       for(int i=0;i<count;i++){
-          String s=scanner.nextLine();
-          System.out.println(s);
-       }
+        for(int i=0;i<count;i++){
+          String[] ss=scanner.nextLine().split(" ");
+          String s =ss[0];
+          int n1=Integer.valueOf(ss[1]);
+          int n2=Integer.valueOf(ss[2]);
+          if(s.equals("AND")){
+             AND();
+          }
+          else if(s.equals("OR")){
+              OR();
+          }
+          else if(s.equals("XOR")){
+              XOR();
+          }
+          else if(s.equals("FLIP")){
+              FLIP();
+          }
+          else if(s.equals("SET")){
+              SET();
+          }
+        }
        
-       scanner.close();
+        scanner.close();        
     }
+    public  static void AND(){
+      System.out.println("AND");
+    }
+    public  static  void OR(){
+     System.out.println("OR");
+    }
+    public  static void XOR(){
+     System.out.println("XOR");
+    }
+    public  static void FLIP(){
+     System.out.println("FLIP");
+    }
+    public  static void SET(){
+     System.out.println("SET");
+    }    
 }
 
