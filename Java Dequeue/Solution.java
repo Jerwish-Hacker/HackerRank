b89cb6fc455b51bac47e920485bad7491c7ac542
Justin <justinraj958@gmail.com>
2026-02-09 23:47:36 +0530

    Deque part-3

--- Java Dequeue/Solution.java
+++ Java Dequeue/Solution.java
@@ -7,12 +7,27 @@ public class Solution {
         Scanner scanner = new Scanner(System.in);
         int length=scanner.nextInt();
         int k=scanner.nextInt(); 
-        int maxUnique =0;
+        int maxUnique =0;        
         Deque deque = new ArrayDeque<>();
-        for(int i=0;i<k;i++){
+        Set<Object> set=new HashSet<>();
+        
+        for(int i=0;i<length;i++){
           int temp=scanner.nextInt();
           deque.add(temp);
         }
-        System.out.println(deque.toString());
+        System.out.println(deque);
+        
+        for(int i=0;i<k;i++){
+          set.add(deque.removeFirst());          
+        }
+        maxUnique= set.size();
+        
+        for(int i =k;i<deque.size-1;i++){
+           
+           
+          if(maxUnique<0){
+            maxUnique = 0;
+          }
+        }        
     }
 }
