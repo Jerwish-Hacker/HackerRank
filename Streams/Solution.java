3d820164830494f3ef516028a1262cc1961f4504
Justin <justinraj958@gmail.com>
2026-02-17 21:43:27 +0530

    Java Streams

---
+++ Streams/Solution.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution{
+    public static void main(String[] args){
+      //List<Integer> nums = Arrays.asList(5, 2, 3, 5, 2, 10, 3, 20);
+      List<Integer> nums=Arrays.asList(5,2,3,77,85,15,35);
+      //List<String> names = Arrays.asList("justin", "raj", "hcl");
+      System.out.println(nums);
+      
+      List<Integer> result = nums.stream()
+          .sorted()
+          .skip(3)
+          .collect(Collectors.toList());
+      System.out.println(result);
+    }
+}
+// I know this following 
+//filter
+//map
+//collect
+//sorted
+//distinct
+//limit
+//skip
