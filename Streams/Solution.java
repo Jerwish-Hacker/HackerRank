520dfb65c5e5b08d0d8dda55318b23d8cbcf88f5
Justin <justinraj958@gmail.com>
2026-02-18 21:17:46 +0530

    Remove duplicates in Java Stream

--- Streams/Solution.java
+++ Streams/Solution.java
@@ -5,15 +5,16 @@ import java.util.stream.Collectors;
 public class Solution{
     public static void main(String[] args){
       //List<Integer> nums = Arrays.asList(5, 2, 3, 5, 2, 10, 3, 20);
-      List<Integer> nums=Arrays.asList(5,2,3,77,85,15,35);
+      //List<Integer> nums=Arrays.asList(5,2,3,77,85,15,35);
       //List<String> names = Arrays.asList("justin", "raj", "hcl");
+      List<Integer> nums = Arrays.asList(5, 2, 3, 77, 85, 15, 35, 2, 77);
       System.out.println(nums);
       
-      long count= nums.stream()
-          .filter(n-> n >10)
-          .count();
+      List<Integer> result = nums.stream()
+          .distinct()
+          .collect(Collectors.toList());
           
-      System.out.println(count);
+      System.out.println(result);
     }
 }
 // I know this following 
