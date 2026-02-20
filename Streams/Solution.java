f191c170e60705f697d6e5fdc36fb0ec7e1c1103
Justin <justinraj958@gmail.com>
2026-02-20 21:21:26 +0530

    Max() in Streams

--- Streams/Solution.java
+++ Streams/Solution.java
@@ -7,16 +7,13 @@ public class Solution{
       //List<Integer> nums = Arrays.asList(5, 2, 3, 5, 2, 10, 3, 20);
       //List<Integer> nums=Arrays.asList(5,2,3,77,85,15,35);
       //List<String> names = Arrays.asList("justin", "raj", "hcl");
-      List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9,10); 
+      List<Integer> nums = Arrays.asList(11,2,3,4,5,6,7,8,9,10); 
       
-      List<Integer> result = nums.stream()
-          .filter(n-> n%2 == 0)
-          .map(n-> n*n)
-          .skip(1)
-          .limit(3)        
-          .collect(Collectors.toList());
+      Optional<Integer> result = nums.stream()
+          .max(Comparator.naturalOrder());
+          
       
-      System.out.println(result);
+      System.out.println(result.get());
     }
 }
 // I know this following 
