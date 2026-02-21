b0218cf6cdc6fc5c9b24145d217e21e713537af6
Justin <justinraj958@gmail.com>
2026-02-21 17:44:50 +0530

    allMatch & noneMatch in Streams

--- Streams/Solution.java
+++ Streams/Solution.java
@@ -7,11 +7,12 @@ public class Solution{
       //List<Integer> nums = Arrays.asList(5, 2, 3, 5, 2, 10, 3, 20);
       //List<Integer> nums=Arrays.asList(5,2,3,77,85,15,35);
       //List<String> names = Arrays.asList("justin", "raj", "hcl");
-      List<Integer> nums = Arrays.asList(11,2,3,4,5,6,7,8,9,10);
+      //List<Integer> nums = Arrays.asList(11,2,3,4,5,6,7,8,9,10);
       //List<Integer> nums = Arrays.asList(2, 4, 6, 8, 10);
+      List<Integer> nums = Arrays.asList(1, 3, 5, 7, 9);
       
-      Boolean result=nums.stream()
-                         .anyMatch(n-> n%2 == 0); 
+      boolean result=nums.stream()
+                         .noneMatch(n-> n%2 == 0); 
       
       System.out.println(result);
     }
@@ -29,3 +30,4 @@ public class Solution{
 //min
 //anyMatch
 //allMatch
+//noneMatch
