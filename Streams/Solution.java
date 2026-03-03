3a74bb03d851c1c916109d84e55ad2a584bb297e
Justin <justinraj958@gmail.com>
2026-03-03 21:52:51 +0530

    Streams Recall

--- Streams/Solution.java
+++ Streams/Solution.java
@@ -4,17 +4,14 @@ import java.util.stream.Collectors;
 
 public class Solution{
     public static void main(String[] args){
-      //List<Integer> nums = Arrays.asList(5, 2, 3, 5, 2, 10, 3, 20);
-      //List<Integer> nums=Arrays.asList(5,2,3,77,85,15,35);
-      //List<String> names = Arrays.asList("justin", "raj", "hcl");
-      //List<Integer> nums = Arrays.asList(11,2,3,4,5,6,7,8,9,10);            
-      //List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
-      List<Integer> nums = Arrays.asList(12, 5, 18, 7, 20, 3, 25);
       
-      List<Integer> result=nums.stream()
-           .filter(n->n>10)
-           .sorted(Comparator.reverseOrder())
-           .collect(Collectors.toList());
+      List<String> names = Arrays.asList("Justin", "Raj", "Arun", "Kiran", "Ravi", "Jo");
+      
+      List<String> result=names.stream()
+            .filter(n->n.length()>3)
+            .map(s-> s.toUpperCase())
+            .sorted(Comparator.naturalOrder())
+            .collect(Collectors.toList());
       System.out.println(result);
     }
 }
