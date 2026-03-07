49784dc277e5e59dac46cd71dbf3063661c91795
Justin <justinraj958@gmail.com>
2026-03-07 23:37:10 +0530

    Reverse String using StringBuilder

--- dfsafdsa/Solution.java
+++ dfsafdsa/Solution.java
@@ -5,22 +5,9 @@ import java.util.stream.Collectors;
 
 public class Solution{
   public static void main(String[] args){
-    String s = "madam";
-    char[] charArr = s.toCharArray(); 
-    int left=0;
-    int right = charArr.length-1;
-    while(left < right){
-      char temp = charArr[left];      
-      charArr[left] = charArr[right];
-      charArr[right] = temp;
-      left++;
-      right--;
-    }
-    if(s.equals(new String(charArr))){
-      System.out.println("Palindrome");
-    }
-    else{
-      System.out.println("Not Palindrome");
-    }
+    String s = "Justin";  
+    StringBuilder sb=new StringBuilder(s);    
+    System.out.println(sb.reverse());
+    
   }
 }
