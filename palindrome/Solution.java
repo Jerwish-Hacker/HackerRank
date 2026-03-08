598147512b65a2ac78015cee48cc2e51f26a649b
Justin <justinraj958@gmail.com>
2026-03-08 17:21:30 +0530

    Palindrome in java

---
+++ palindrome/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class  Solution{
+  public static void main(String[] args){
+    String s= "madam";
+    char[] cArr = s.toCharArray();
+    int left=0;
+    int right = s.length()-1;    
+    while(left < right){    
+      char c = cArr[left];
+      cArr[left] = cArr[right];
+      cArr[right] = c;
+      left++;
+      right--;
+    }    
+    System.out.println(s.equals(new String(cArr)));    
+  }
+}
