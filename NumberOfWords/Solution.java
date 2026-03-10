1543fb6a877ff8bcf946a6d02d3b92fa9c1066ea
Justin <justinraj958@gmail.com>
2026-03-10 21:12:54 +0530

    Find the number of words in String

---
+++ NumberOfWords/Solution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+
+public class Solution{
+  public static void main(String[] args){
+    String s= "Java is powerfull";
+    String[] arr=s.split(" ");
+    System.out.println(arr.length);    
+  }
+}
